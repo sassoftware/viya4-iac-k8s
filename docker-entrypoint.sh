8aa4eb849ad1148d581c89e79f29b7711b01239c
jarpat <jay.patel@sas.com>
2023-06-16 13:56:01 -0400

    resolve ShellCheck warnings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,4 @@ set -e
 echo "viya4-iac-k8s:x:$(id -u):$(id -g)::/viya4-iac-k8s:/bin/bash" >> /etc/passwd
 echo "viya4-iac-k8s:x:$(id -G | cut -d' ' -f 2):" >> /etc/group
 
-exec /viya4-iac-k8s/oss-k8s.sh $@
+exec /viya4-iac-k8s/oss-k8s.sh "$@"
